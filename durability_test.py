6a60f66b48499eae48d98f4d345c9643b4192351
fearmansighah <66765258+fearmansighah@users.noreply.github.com>
2023-04-11 01:50:03 -0700

    showiing original functions

--- durability_test.py
+++ durability_test.py
@@ -1,10 +1,5 @@
 import os
 import time
-import board
-import adafruit_am2320
-import paho.mqtt.client as mqtt
-
-# to run: sudo python3 duability_test.py
 
 
 def measure_temp():
@@ -29,28 +24,13 @@ def measure_core():
     return (freq)
 
 
-# create the I2C shared bus
-i2c = board.I2C()  # uses board.SCL and board.SDA
-am = adafruit_am2320.AM2320(i2c)
-timenow = 0
-mqttBroker = "mqtt.eclipseprojects.io"
-mqttClient = mqtt.Client("crwn_pi")
-topic = 'durability_test'
 
 while True:
-    # timestamp, core temp, # arm freq, # sensor temp, # sensor hum
-    #pr(timestamp(), measure_temp(), measure_freq(), am.temperature, am.relative_humdiity)
-    measurements = f'{timenow}, {measure_temp()}, {measure_core()}, {measure_arm()}, {am.temperature}, {am.relative_humidity}'
-
     measurements = f'{timenow}, {measure_temp()}, {measure_core()}, {measure_arm()}'
     print(measurements, '\n')
 
     with open('readings.txt', "a") as file:
         file.write(measurements + "\n")
-
-    mqttClient.connect(mqttBroker)
-
-    mqttClient.publish(topic, measurements)
 
     time.sleep(1)
     timenow += 1
